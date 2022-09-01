3252a74edc9ba6e9e2ab8554e9f30f877f8fa0bc
Den <potarykindenis@gmail.com>
2022-09-01 13:00:56 +0300

    Updates

---
+++ precache-manifest.9cd170e0f7e65a47ca492326d52ac8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b02afc7bce27b56312d97e22ba4cc11",
+    "url": "/DenRainmen/Counter-react-rev3.git/index.html"
+  },
+  {
+    "revision": "8074ee4899fdb6a713ad",
+    "url": "/DenRainmen/Counter-react-rev3.git/static/css/main.169c0c70.chunk.css"
+  },
+  {
+    "revision": "e5d9b85709b768a840d6",
+    "url": "/DenRainmen/Counter-react-rev3.git/static/js/2.cf929752.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/DenRainmen/Counter-react-rev3.git/static/js/2.cf929752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8074ee4899fdb6a713ad",
+    "url": "/DenRainmen/Counter-react-rev3.git/static/js/main.06f408c8.chunk.js"
+  },
+  {
+    "revision": "b36be12ce0881bff0d49",
+    "url": "/DenRainmen/Counter-react-rev3.git/static/js/runtime-main.8aa761be.js"
+  }
+]);
